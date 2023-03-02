6370ddb26af4082ffbc5c9b41c72212330ff2e60
TxuanThang <xxuanthang25@gmail.com>
2023-03-02 14:35:35 +0700

    upload single file

--- src/controller/home.js
+++ src/controller/home.js
@@ -1,4 +1,7 @@
 import pool from "../configs/ConnectDB.js";
+import multer from "multer";
+
+
 export const gethome = async (req, res) => {
     const [rows, fields] = await pool.execute('SELECT * FROM `products`'); // để dùng await thì cần dùng async
     return res.render('sever.ejs', { dataProducts: rows });
@@ -31,9 +34,33 @@ export const EditProduct = async (req, res) => {
 export const UpdateProduct = async (req, res) => {
     let {NameProduct,Image,Price,Description,id} = req.body;
     await pool.execute("update products set NameProduct=?,Image=?,Price=?,Description=? where ProductID=?", [NameProduct, Image, Price,Description,id])
-
-
     return res.redirect('/');
 }
-export default { gethome, getDetailPage, AddProducts, CreateProduct,DeleteProducts, EditProduct, UpdateProduct };
+export const Uploadfile = async (req, res) => {
+    return res.render('Upload.ejs')
+}
+
+const upload = multer().single('upload');  // upload biết đang dùng multer và nói cho nó 1 cái tên
+
+export const Uploadsingle = async (req, res) => {
+    // 'upload' nó sẽ map với tên của ô input ở uploadfile
+    upload(req, res, function(err) {
+        // req.file dùng để chưa thông tin file đã tải lên
+        // req.body chưa thông tin bất kì của văn nếu có
+
+        if (req.fileValidationError) {
+            return res.send(req.fileValidationError);
+        }
+        // nếu không gửi file nó sẽ báo lỗi
+        else if (!req.file) {
+            return res.send('Please select an image to upload');
+        }
+        // nếu có lỗi của thư viện multer thì nó sẽ gửi luôn lỗi của thư viện
+        else if (err instanceof multer.MulterError) {
+            return res.send(err);
+        }
+        res.send(`You have uploaded this image: <hr/><img src="/img/${req.file.filename}" width="100"><hr /><a href="./uploadfile">Upload another image</a>`);
+    });
+}
+export default { gethome, getDetailPage, AddProducts, CreateProduct,DeleteProducts, EditProduct, UpdateProduct, Uploadfile, Uploadsingle };
 // 
--- src/router/web.js
+++ src/router/web.js
@@ -1,4 +1,7 @@
 import express from 'express';
+import multer from 'multer';
+import path from 'path';
+import appRoot from 'app-root-path';
 import {gethome} from '../controller/home.js';
 import {getDetailPage} from '../controller/home.js';
 import { AddProducts } from '../controller/home.js';
@@ -6,8 +9,33 @@ import {CreateProduct} from '../controller/home.js';
 import {DeleteProducts} from '../controller/home.js';
 import {EditProduct} from '../controller/home.js';
 import {UpdateProduct} from '../controller/home.js';
-
+import {Uploadfile} from '../controller/home.js';
+import {Uploadsingle} from '../controller/home.js';
+// var appRoot = require('app-root-path');
 let router = express.Router();
+
+const storage = multer.diskStorage({         // dùng để cho nó biết nó lưu file ở đâu
+    destination: function(req, file, cb) {
+        cb(null, appRoot + '/src/public/img/');
+    },
+
+    //  dùng để lấy tên ảnh +  thời gian hiện tại dưới dạng string + đường dẫn nơi lưu trữ
+    filename: function(req, file, cb) {
+        cb(null, 'Anhmoi' + '-' + Date.now() + path.extname(file.originalname));
+    }
+});
+    
+const imageFilter = function(req, file, cb) { //Khi truyền vào ta có 1 requets, 1 file and 1 callback
+    // Accept images only
+    if (!file.originalname.match(/\.(jpg|JPG|jpeg|JPEG|png|PNG|gif|GIF)$/)) { // định dạng kiểu file image
+        req.fileValidationError = 'Only image files are allowed!';
+        return cb(new Error('Only image files are allowed!'), false); // khi có lỗi sẽ truyền vào cái lỗi là Error và truyền vào giá trị false
+    }
+    cb(null, true); // không có lỗi nó sẽ truyền vào null và true
+};
+
+let upload = multer({ storage: storage, fileFilter: imageFilter }).single('upload'); // storage đã được định nghĩ ở trên, single định nghĩ chỉ upload 1 file
+
 const initRoute = (app) => {
     router.get('/', gethome);
     router.get('/Products/detail/:ProductID', getDetailPage); // router parameters sử dụng để nắm bắt các giá trị được chỉ định tại vị trí của chúng trong URL
@@ -16,6 +44,9 @@ const initRoute = (app) => {
     router.post('/deleteProduct', DeleteProducts);  
     router.post('/UpdateProduct', UpdateProduct);
     router.post('/createProduct', CreateProduct);
+    // upload file
+    router.get('/uploadfile', Uploadfile);
+    router.post('/Uploadsingle',upload,Uploadsingle)
     return app.use('/', router) // tham số đầu là tiền tố ví dụ có thể dùng: /abc
 }
 export default initRoute;
